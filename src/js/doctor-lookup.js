93a06de2d6fb107f61aed178f65b5f625a8889c8
Maggie Summers <summers.maggie@gmail.com>
2018-03-23 12:01:32 -0700

    add front end logic to display doctors by medical condition

--- src/js/doctor-lookup.js
+++ src/js/doctor-lookup.js
@@ -18,7 +18,7 @@ export class doctorLookup {
 
   getSpecialities(medicalIssue, displaySpecialities) {
     const apiKey = process.env.exports.apiKey;
-
+    console.log(medicalIssue);
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?conditions=${medicalIssue}&location=45.523%2C-122.676%2C500&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${apiKey}`).then(function(response)
   {
     displaySpecialities(response);
--- src/main.js
+++ src/main.js
@@ -28,17 +28,43 @@ const displayDoctors = function(response) {
   }
 }
 
+const displaySpecialities = function(response) {
+  if (response.data.length == 0) {
+    $('#doctor-results').text("There are no doctors that match your search.")
+  } else {
+    for(let i = 0; i < response.data.length; i++) {
+
+      let firstName = response.data[i].profile.first_name;
+      let lastName = response.data[i].profile.last_name;
+
+      let street = response.data[i].practices[i].visit_address.street;
+      let city = response.data[i].practices[i].visit_address.city;
+      let state = response.data[i].practices[i].visit_address.state;
+      let zipcode = response.data[i].practices[i].visit_address.zip;
+
+      let newPatients =  
+      response.data[i].practices[i].accepts_new_patients;
+
+      let phoneNumber =
+      response.data[i].practices[i].phones[i].number;
+
+      let website = (website === undefined) ? "None" : response.data[i].practices[i].website;
+
+      $('#doctor-results').append(" " + '<p>' + firstName + " " + lastName + ", " + street + " " + city + ", " + state + " " + zipcode + '</p>' + '<p>' + "<strong>Accepts new patients?</strong>" + " " + newPatients + '</p>' + "<strong>Phone Number:</strong>" + " " + phoneNumber + '</p>' + "<strong>Website:</strong>" + " " + website + '</p>' + "<hr>");
+    }
+  }
+}
+
 $(document).ready(function() {
   $("#doctor-search").submit(function(event) {
     event.preventDefault();
-
     const name = $("#name").val();
     const medicalIssue = $("#medical-issue").val();
-
-    let newDoctorSearch = new doctorLookup(name, medicalIssue);
     $("#doctor-search").hide();
 
-    newDoctorSearch.getDoctors(name, displayDoctors);
-  })
+    let newDoctorSearch = new doctorLookup(name, medicalIssue);
 
+    newDoctorSearch.getDoctors(name, displayDoctors);
+    newDoctorSearch.getSpecialities(medicalIssue, displaySpecialities)
+  })
 })
