55077263a7c98284fec02390a63998528497ef7b
Maggie Summers <summers.maggie@gmail.com>
2018-03-23 09:46:05 -0700

    add API functionality to search by doctor name

--- src/js/doctor-lookup.js
+++ src/js/doctor-lookup.js
@@ -4,13 +4,12 @@ export class doctorLookup {
     this.medicalIssue = medicalIssue;
   }
 
-  getDoctors(name) {
+  getDoctors(name, displayDoctors) {
     const apiKey = process.env.exports.apiKey;
-    console.log(exports.apiKey);
 
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`).then(function(response)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`).then(function(response)
   {
-    // displayDoctors(response);
+    displayDoctors(response);
   })
     .fail(function(error) {
       $('.api-error').text(`There was an error processing your request: ${error.responseTest}. Please try again.`)
--- src/main.js
+++ src/main.js
@@ -1,2 +1,34 @@
 import { doctorLookup } from './js/doctor-lookup.js';
 import './styles.css'
+
+const displayDoctors = function(response) {
+  // if (response.data.profile == 0) {
+  //   console.log(response.data.practices);
+  //   $('#doctor-results').text("There are no doctors that match your search.")
+  // } else {
+  console.log(response.data.length);
+    for(let i = 0; i < response.data.length; i++) {
+      let firstName = response.data[i].profile.first_name;
+      let lastName = response.data[i].profile.last_name;
+      console.log(firstName);
+      $('#doctor-results').append(" " + '<li>' + firstName + " " + lastName + '</li>');
+    }
+  }
+
+
+$(document).ready(function() {
+  $("#doctor-search").submit(function(event) {
+    event.preventDefault();
+
+    const name = $("#name").val();
+    const medicalIssue = $("#medical-issue").val();
+    console.log(name);
+    console.log(medicalIssue);
+
+    let newDoctorSearch = new doctorLookup(name, medicalIssue);
+    $("#doctor-search").hide();
+
+    newDoctorSearch.getDoctors(name, displayDoctors);
+  })
+
+})
