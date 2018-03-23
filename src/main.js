aad3fd3601e6454b73ad32e5ae609cd78e162a8f
Maggie Summers <summers.maggie@gmail.com>
2018-03-23 15:02:57 -0700

    experiment with adding a second loop within the outer loop to circle through JSON response

--- src/main.js
+++ src/main.js
@@ -6,24 +6,32 @@ const displayDoctors = function(response) {
     $('#doctor-results').text("There are no doctors that match your search.");
   } else {
     for(let i = 0; i < response.data.length; i++) {
-
+      console.log(response.data.length);
       let firstName = response.data[i].profile.first_name;
       let lastName = response.data[i].profile.last_name;
-      console.log(firstName);
-      let street = response.data[i].practices[i].visit_address.street;
-      let city = response.data[i].practices[i].visit_address.city;
-      let state = response.data[i].practices[i].visit_address.state;
-      let zipcode = response.data[i].practices[i].visit_address.zip;
+      
+      for(let j = 0; j < response.data[0].practices[i]; j++) {
 
-      let newPatients =
-      response.data[i].practices[i].accepts_new_patients;
+        console.log(response.data[0].practices[i]);
 
-      let phoneNumber =
-      response.data[i].practices[i].phones[i].number;
 
-      let website = (website === undefined) ? "none" : response.data[i].practices[i].website;
+        let street = response.data[i].practices[j].visit_address.street;
+        let city = response.data[i].practices[j].visit_address.city;
 
-      $('#doctor-results').append(" " + '<p>' + firstName + " " + lastName + ", " + street + " " + city + ", " + state + " " + zipcode + '</p>' + '<p>' + "<strong>Accepts new patients?</strong>" + " " + newPatients + '</p>' + "<strong>Phone Number:</strong>" + " " + phoneNumber + '</p>' + "<strong>Website:</strong>" + " " + website + '</p>' + "<hr>");
+        let state = response.data[i].practices[j].visit_address.state;
+
+        let zipcode = (zipcode === undefined) ? "None" : response.data[i].practices[j].visit_address.zip;
+
+        let newPatients = (newPatients === undefined) ? "None" :
+        response.data[i].practices[j].accepts_new_patients;
+
+        let phoneNumber = (phoneNumber === undefined) ? "None" :
+        response.data[i].practices[j].phones[i].number;
+
+        let website = (website === undefined) ? "None" : response.data[i].practices[j].website;
+
+        $('#doctor-results').append(" " + '<p>' + firstName + " " + lastName + ", " + street + " " + city + ", " + state + " " + zipcode + '</p>' + '<p>' + "<strong>Accepts new patients?</strong>" + " " + newPatients + '</p>' + "<strong>Phone Number:</strong>" + " " + phoneNumber + '</p>' + "<strong>Website:</strong>" + " " + website + '</p>' + "<hr>");
+      }
     }
   }
 }
@@ -34,7 +42,6 @@ const displaySpecialities = function(response) {
     $('#doctor-results').text("There are no doctors that match your search.")
   } else {
     for(let i = 0; i < response.data.length; i++) {
-
       let firstName = response.data[i].profile.first_name;
       let lastName = response.data[i].profile.last_name;
 
